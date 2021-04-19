81f3c63b2bd8a1a467584514c77fdf4335e22687
Morgan Bradford <morganjbradford95@gmail.com>
2021-04-19 13:34:06 -0500

    add import statements
    Co-authored-by: Joseph Giunta <jgnohack@gmail.com>

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import Header from "./Header";
+import About from "./About";
+import Feed from "./Feed";
 
 const App = () => {
   return (
     <>
       <Header />
-      <Profile />
-      {/* <About />
+      {/* <Profile /> */}
+      <About />
       <Feed />
-      <Suggested /> */}
+      {/* <Suggested /> */}
     </>
   );
 }
