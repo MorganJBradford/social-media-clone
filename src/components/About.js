b50fb46e6fc9e963fbd3ee8b3f822e970f7b1ec0
Morgan Bradford <morganjbradford95@gmail.com>
2021-04-19 13:04:02 -0500

    add About component with lorum ipsum
    Co-authored-by: Joseph Giunta <jgnohack@gmail.com>

---
+++ src/components/About.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const About = () => {
+return (
+    <p>Aenean ipsum felis, suscipit quis augue eu, iaculis vestibulum felis. Quisque mollis leo in dui pretium, a vulputate tortor tincidunt. Praesent mauris diam, posuere quis pulvinar egestas, ultrices sit amet tortor. Phasellus fringilla dui sed dui euismod efficitur. Suspendisse malesuada fermentum velit et fringilla. Morbi lacinia feugiat commodo. Etiam faucibus aliquet dui eu finibus. Pellentesque placerat vitae libero in dignissim.</p>
+  );
+}
