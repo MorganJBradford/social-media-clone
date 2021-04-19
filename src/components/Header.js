d9db00d4ad7e00a9aa8d8ebdab8bd87927454dc5
Morgan Bradford <morganjbradford95@gmail.com>
2021-04-19 13:01:19 -0500

    Add buttons and input for search bar
    Co-authored-by: Joseph Giunta <jgnohack@gmail.com>

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <button>Home</button>
+      <button>Notifications</button>
+      <button>Messages</button>
+      <input placeholder="Search"></input>
+    </>
+  );
+}
+
+export default Header;
