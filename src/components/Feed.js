dc12b1e45a8f43360959efe41892b8e04b505e3e
Morgan Bradford <morganjbradford95@gmail.com>
2021-04-19 13:02:05 -0500

    add objects for posts; input for a tweet; map for each post
    Co-authored-by: Joseph Giunta <jgnohack@gmail.com>

---
+++ src/components/Feed.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Post from "./Post";
+
+const postList = [
+  {
+    names: "Greg",
+    content: "Good morning!"
+  },
+  {
+    names: "Haley",
+    content: "Hello world!"
+  },
+  {
+    names: "Billy-Bob",
+    content: "How's it going?"
+  }
+];
+
+const Feed = () => {
+  return (
+    <>
+      <input placeholder="What's happening?"></input>
+      <hr/>
+      {postList.map((post, index) =>
+        <Post names={post.name}
+          post={post.content}
+          key={index}/>
+      )}
+    </>
+  );
+}
